d9dc33277aa5ab937c6d1cab2553fa3bbc9a73b0
WenTao-Rong <rwentao@126.com>
2019-09-23 22:26:28 +0800

    busheaders

---
+++ busheaders.h
@@ -0,0 +1,21 @@
+#ifndef __BUSHEADER__
+#define __BUSHEADER__
+#include <iostream>
+#include "./include/tool/tool.h"
+#include "./include/blob/blob.h"
+#include "./include/blob/BlobResult.h"
+#include "opencv2/imgproc/imgproc.hpp"
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv/highgui.h"
+#include <fstream>
+#include <opencv2/opencv.hpp>
+#include <opencv2/core/types_c.h>
+#include "opencv/cxcore.h"
+#include "opencv/cv.h"
+#include <vector>
+#include <string>
+#include "./include/basic/RAMImage.h"
+#include "./include/glcm/glcm.h"
+#include "./include/glrlm/GLRLMFEATURES2D.h"
+#include <fstream>
+#endif // __BUSHEADER__
